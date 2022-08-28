aac183319f1ac1af4898b2bb080518853ba3dc24
Kevin Batdorf <kbat82@gmail.com>
2022-08-28 16:08:23 -0400

    Update version numbers

--- code-block-pro.php
+++ code-block-pro.php
@@ -4,7 +4,7 @@
  * Description:       Code highlighting powered by the VS Code engine
  * Requires at least: 6.0
  * Requires PHP:      7.0
- * Version:           1.2.1
+ * Version:           1.2.2
  * Author:            Kevin Batdorf
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
