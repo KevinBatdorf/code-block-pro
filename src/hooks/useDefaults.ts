0b529d06396ea223f878f118c4986c8de325841f
Kevin Batdorf <kbat82@gmail.com>
2022-11-17 04:23:17 -0500

    Run defaults check only once

--- src/hooks/useDefaults.ts
+++ src/hooks/useDefaults.ts
@@ -1,4 +1,4 @@
-import { useEffect } from '@wordpress/element';
+import { useEffect, useRef } from '@wordpress/element';
 import { Theme } from 'shiki';
 import { useGlobalStore } from '../state/global';
 import { useThemeStore } from '../state/theme';
@@ -32,51 +32,61 @@ export const useDefaults = ({
         previousDisablePadding,
         previousLineNumbers,
     } = useThemeStore();
+    const once = useRef(false);
 
     useEffect(() => {
+        if (once.current) return;
         if (copyButton !== undefined || !previousSettings.copyButton) return;
         setAttributes({ copyButton: previousSettings.copyButton });
     }, [previousSettings, copyButton, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (theme || !previousTheme) return;
         setAttributes({ theme: previousTheme as Theme });
     }, [previousTheme, theme, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (fontSize || !previousFontSize) return;
         setAttributes({ fontSize: previousFontSize });
     }, [previousFontSize, fontSize, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (fontFamily || fontFamily === '' || previousFontFamily === undefined)
             return;
         setAttributes({ fontFamily: previousFontFamily });
     }, [previousFontFamily, fontFamily, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (lineHeight || !previousLineHeight) return;
         setAttributes({ lineHeight: previousLineHeight });
     }, [previousLineHeight, lineHeight, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (headerType || !previousHeaderType) return;
         setAttributes({ headerType: previousHeaderType });
     }, [previousHeaderType, headerType, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (footerType !== undefined || previousFooterType === undefined)
             return;
         setAttributes({ footerType: previousFooterType });
     }, [previousFooterType, footerType, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (clampFonts !== undefined || previousClampFonts === undefined)
             return;
         setAttributes({ clampFonts: previousClampFonts });
     }, [previousClampFonts, clampFonts, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (
             disablePadding !== undefined ||
             previousDisablePadding === undefined
@@ -86,8 +96,15 @@ export const useDefaults = ({
     }, [previousDisablePadding, disablePadding, setAttributes]);
 
     useEffect(() => {
+        if (once.current) return;
         if (lineNumbers !== undefined || previousLineNumbers === undefined)
             return;
         setAttributes({ lineNumbers: previousLineNumbers });
     }, [previousLineNumbers, lineNumbers, setAttributes]);
+
+    useEffect(() => {
+        requestAnimationFrame(() => {
+            once.current = true;
+        });
+    }, []);
 };
