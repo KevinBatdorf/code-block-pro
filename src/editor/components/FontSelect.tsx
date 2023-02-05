9a881de2aec53d31d98d4fb11c8d4724850e8e10
Kevin Batdorf <kbat82@gmail.com>
2023-02-05 08:55:12 -0500

    Update help text

--- src/editor/components/FontSelect.tsx
+++ src/editor/components/FontSelect.tsx
@@ -120,10 +120,7 @@ export const FontFamilySelect = ({
         <SelectControl
             id="code-block-pro-font-family"
             label={__('Font Family', 'code-block-pro')}
-            help={__(
-                'Fonts only show on the front end of your site.',
-                'code-block-pro',
-            )}
+            help={__('Fonts only render on the frontend.', 'code-block-pro')}
             value={value}
             onChange={onChange}
             options={Object.entries(fonts).map(([value, label]) => ({
