c5e2fff402fa1e1e731880711bc049daad9cfccb
Kevin Batdorf <kbat82@gmail.com>
2023-04-08 21:38:22 -0400

    Fix height patterns with custom themes

--- src/editor/components/seemore/BlockRight.tsx
+++ src/editor/components/seemore/BlockRight.tsx
@@ -1,22 +1,34 @@
 import { __ } from '@wordpress/i18n';
 import { colord, AnyColor } from 'colord';
 import { Attributes } from '../../../types';
+import { findBackgroundColor, findTextColor } from '../../../util/colors';
 
-export const BlockRight = ({
-    bgColor,
-    textColor,
-    seeMoreString,
-    disablePadding,
-    context,
-    footerType,
-}: Partial<Attributes> & { context?: string }) => {
-    const backgroundColor = colord(bgColor as AnyColor).toHex();
+export const BlockRight = (
+    attributes: Partial<Attributes> & { context?: string },
+) => {
+    const {
+        bgColor,
+        textColor,
+        seeMoreString,
+        disablePadding,
+        context,
+        footerType,
+    } = attributes;
+    let backgroundColor = colord(bgColor as AnyColor).toHex();
     const textC = colord(textColor as AnyColor);
-    const color = textC.isDark()
+    let color = textC.isDark()
         ? textC.lighten(0.05).toHex()
         : textC.darken(0.05).toHex();
     const hasFooter = footerType !== 'none';
     const inEditor = context === 'editor';
+
+    if (textColor?.startsWith('var(')) {
+        color = findTextColor(attributes) ?? textColor;
+    }
+    if (bgColor?.startsWith('var(')) {
+        backgroundColor = findBackgroundColor(attributes) ?? bgColor;
+    }
+
     return (
         <div
             className="cbp-see-more-container"
