407ecc5c79f7bca305a1318f5ce85f58a564c8d1
Kevin Batdorf <kbat82@gmail.com>
2023-10-08 00:50:31 -0400

    Update test

--- cypress/e2e/permissions.cy.js
+++ cypress/e2e/permissions.cy.js
@@ -1,6 +1,13 @@
 beforeEach(() => {
     cy.resetDatabase();
     cy.clearBrowserStorage();
+    cy.intercept(
+        'GET',
+        '/index.php?rest_route=%2Fcode-block-pro%2Fv1%2Fcan-save-html&_locale=user',
+        {
+            body: false,
+        },
+    ).as('canSaveHtml');
     cy.loginUser();
     cy.visitNewPostEditor();
 });
@@ -11,14 +18,17 @@ afterEach(() => {
 context('Permissions', () => {
     it('Warning shows when cap is missing', () => {
         cy.addBlock('kevinbatdorf/code-block-pro');
+        cy.wait('@canSaveHtml');
         cy.getPostContent('.wp-block[class$="code-block-pro"]')
             .invoke('html')
-            .should('contain', 'capability is required to edit this block');
+            .should('contain', 'capability is required');
     });
 
     it('Prevents updating attributes', () => {
         cy.addBlock('kevinbatdorf/code-block-pro');
         cy.openSideBarPanel('Line Settings');
+
+        cy.wait('@canSaveHtml');
 
         // ported from lines spec with the final assertions updated
         cy.get('[data-cy="show-line-numbers"]')
